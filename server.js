a6624a3946b46b6266500c530db0b3128a0236de
Purvasha <purvasha1013@gmail.com>
2024-05-07 12:45:58 +1000

    add socket

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cardRoutes = require('./routes/index');
 
 const app = express();
+const http = require('http').createServer(app); // Create HTTP server
 
 mongoose.connect('mongodb+srv://purvasha1013:Imp_560062@sit-725.b8fxacb.mongodb.net/');
 const db = mongoose.connection;
@@ -15,9 +16,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.use('/', cardRoutes);
 
+const io = require('socket.io')(http); 
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    // User added
+    socket.emit('hello', 'Hello user, welcome!');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    setInterval(() => {
+        socket.emit('number', parseInt(Math.random() * 10));
+    }, 1000);
+
+    socket.on('number', (msg) => {
+        console.log('Random number: ' + msg);
+    });
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
